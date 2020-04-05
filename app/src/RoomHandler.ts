c1de5de2b4cb00bb780474bada7c6dd80e38ac8d
Oliver Kwast <o.kwast@lokalportal.de>
2020-04-05 18:25:57 +0200

    🍹

--- app/src/RoomHandler.ts
+++ app/src/RoomHandler.ts
@@ -1,7 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import UUID from 'pure-uuid';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
-import { Message, MessageType } from '../shared/model/Message';
+import {
+  JoinRequestMessage,
+  Message,
+  MessageType,
+  SetNameMessage,
+  SetVoteValueMessage,
+} from '../shared/model/Message';
 import { Room } from '../shared/model/Room';
+import { VoteValue } from '../shared/model/User';
+
+import { useUrlParam } from './UrlHandler';
 
 // TODO: Connect to Heroku 🧙‍♂️
 // const SERVER_HOST = __DEV__ ? 'ws://localhost:8999' : '';
@@ -14,9 +24,15 @@ const INIT_ROOM: Room = {
   admin: undefined,
 };
 
-export const useRoom = () => {
+export const useRoom = (userName: string) => {
   const ws = useRef(new WebSocket(SERVER_HOST)).current;
   const [room, setRoom] = useState(INIT_ROOM);
+
+  userName = userName || 'Guest';
+
+  const roomId = useUrlParam('roomId') || new UUID(4).toString();
+
+  const send = useCallback((msg: any) => ws.send(JSON.stringify(msg)), [ws]);
 
   useEffect(() => {
     ws.onopen = () => {
@@ -24,6 +40,15 @@ export const useRoom = () => {
         // eslint-disable-next-line no-console
         console.log('Opened WebSocket connection');
       }
+
+      const joinRequest: JoinRequestMessage = {
+        type: MessageType.JoinRequest,
+        roomId,
+        payload: {
+          name: userName,
+        },
+      };
+      send(joinRequest);
     };
     ws.onmessage = (event) => {
       const msg: Message = JSON.parse(event.data);
@@ -52,8 +77,31 @@ export const useRoom = () => {
       }
       // setErrorMessage({ title: 'Oops!', message: 'Something went wrong...' });
     };
-    return () => ws.close();
-  }, [ws]);
+  }, [roomId, send, userName, ws]);
 
-  return { send: (msg: any) => ws.send(JSON.stringify(msg)), room };
+  useEffect(() => {
+    if (ws.readyState === WebSocket.OPEN) {
+      const updateNameRequest: SetNameMessage = {
+        type: MessageType.SetName,
+        roomId,
+        payload: {
+          name: userName,
+        },
+      };
+      send(updateNameRequest);
+    }
+  }, [roomId, send, userName, ws.readyState]);
+
+  const vote = (voteValue: VoteValue) => {
+    const msg: SetVoteValueMessage = {
+      type: MessageType.SetVoteValue,
+      roomId,
+      payload: {
+        voteValue,
+      },
+    };
+    send(msg);
+  };
+
+  return { vote, room };
 };
--- app/src/Root.tsx
+++ app/src/Root.tsx
@@ -1,36 +1,14 @@
-import UUID from 'pure-uuid';
 import React, { useCallback, useState } from 'react';
 import { StatusBar, Text, View } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
-
-import { JoinRequestMessage, MessageType } from '../shared/model/Message';
+import { TextInput } from 'react-native-paper';
 
 import { Results } from './Results';
 import { useRoom } from './RoomHandler';
-import { useUrlParam } from './UrlHandler';
 
 export const Root: React.FC = () => {
   const [userName, setUserName] = useState<string>('');
 
-  const { send, room } = useRoom();
-
-  const roomId = useUrlParam('roomId');
-
-  const joinRoom = useCallback(() => {
-    // TODO: Update this to be more exhaustive
-    const isValidUserName = !!userName || userName.length > 0;
-
-    if (isValidUserName) {
-      const joinRequest: JoinRequestMessage = {
-        type: MessageType.JoinRequest,
-        roomId: roomId || new UUID(4).toString(),
-        payload: {
-          name: userName,
-        },
-      };
-      send(joinRequest);
-    }
-  }, [userName, roomId, send]);
+  const { vote, room } = useRoom(userName);
 
   return (
     <View
@@ -42,18 +20,13 @@ export const Root: React.FC = () => {
         paddingTop: (StatusBar.currentHeight ?? 0) + 16,
       }}
     >
-      <Text>{`Room id: ${roomId || room.id}`}</Text>
+      <Text>{`Room id: ${room.id}`}</Text>
       <View style={{ width: '50%' }}>
         <TextInput
           value={userName}
           label={'Username'}
           onChangeText={setUserName}
         />
-      </View>
-      <View>
-        <Button mode="contained" testID={'join-room-button'} onPress={joinRoom}>
-          <Text>{'Join room'}</Text>
-        </Button>
       </View>
       <Results users={Object.values(room.users)} />
     </View>

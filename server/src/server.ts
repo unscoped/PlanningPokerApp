59abece5019f9d2f6f9e36d0427a19c920f5bf9c
Oliver Kwast <o.kwast@lokalportal.de>
2020-05-03 15:23:23 +0200

    Filter vote values when not everyone voted

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,20 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as WebSocket from 'ws';
-import { Room } from './shared/model/Room'
-import { User } from 'shared/model/User';
-import {Message, MessageType, JoinResponseMessage, RoomUpdateMessage} from './shared/model/Message'
-import Uuid from 'pure-uuid';
-import { stringify } from 'querystring';
+import * as express from "express";
+import * as http from "http";
+import * as WebSocket from "ws";
+import { Room } from "./shared/model/Room";
+import { User } from "shared/model/User";
+import {
+  Message,
+  MessageType,
+  JoinResponseMessage,
+  RoomUpdateMessage,
+} from "./shared/model/Message";
+import Uuid from "pure-uuid";
 
-const rooms: {[key: string]: Room} = {}
-let users: {[key: string]: WebSocket} = {}
+const rooms: { [key: string]: Room } = {};
+let users: { [key: string]: WebSocket } = {};
 
-const port = process.env.PORT || 8999;
+const port = process.env.NODE_ENV === "production" ? 13579 : 8999;
 
 const app = express();
 
@@ -20,54 +24,74 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+const filterValuesFromRoom = (room: Room): Room => {
+  return {
+    ...room,
+    users: Object.fromEntries(
+      Object.entries(room.users).map(([key, user]) => [
+        key,
+        { ...user, voteValue: user.voteValue ? "hidden" : undefined },
+      ])
+    ),
+  };
+};
+
 const updateRoom = (roomId: string) => {
   const room = rooms[roomId];
+
+  const everyoneVoted = Object.values(room.users).every(
+    (user) => user.voteValue
+  );
 
   const msg: RoomUpdateMessage = {
     type: MessageType.RoomUpdate,
     roomId: room.id,
     payload: {
-      room
-    }
-  }
+      room: everyoneVoted ? room : filterValuesFromRoom(room),
+    },
+  };
 
   Object.values(room.users).forEach((user) => {
-    users[user.id].send(JSON.stringify(msg))
-  })
-}
+    users[user.id].send(JSON.stringify(msg));
+  });
+};
 
-const filterUserFromRoom = (userId: string, room: Room): Room => ({...room,
-    users: Object.entries(room.users)
-    .filter(([uId]) => uId !== userId)
-    .reduce<{[key: string]: User}>((room, [uId, user]) => ({...room, [uId]: user}), {})
-  })
+// const filterUserFromRoom = (userId: string, room: Room): Room => ({
+//   ...room,
+//   users: Object.entries(room.users)
+//     .filter(([uId]) => uId !== userId)
+//     .reduce<{ [key: string]: User }>(
+//       (room, [uId, user]) => ({ ...room, [uId]: user }),
+//       {}
+//     ),
+// });
 
-wss.on('connection', (ws: WebSocket) => {
-  const userId = new Uuid(4).format()
-  
+wss.on("connection", (ws: WebSocket) => {
+  const userId = new Uuid(4).format();
+
   //connection is up, let's add a simple simple event
-  ws.on('message', (message: string) => {
+  ws.on("message", (message: string) => {
     //log the received message and send it back to the client
-    console.log('received: %s', message);
-    
+    console.log("received: %s", message);
+
     try {
-      const msg: Message = JSON.parse(message)
-      switch(msg.type) {
+      const msg: Message = JSON.parse(message);
+      switch (msg.type) {
         case MessageType.JoinRequest: {
-          if(!rooms[msg.roomId]) {
+          if (!rooms[msg.roomId]) {
             rooms[msg.roomId] = {
               id: msg.roomId,
               users: {},
               name: msg.roomId,
               admin: undefined,
-            }
+            };
           }
 
           rooms[msg.roomId].users[userId] = {
             id: userId,
             userName: msg.payload.name,
             voteValue: undefined,
-          }
+          };
           users[userId] = ws;
 
           const response: JoinResponseMessage = {
@@ -75,63 +99,61 @@ wss.on('connection', (ws: WebSocket) => {
             roomId: msg.roomId,
             payload: {
               userId,
-            }
-          }
+            },
+          };
 
-          ws.send(JSON.stringify(response))
-          
+          ws.send(JSON.stringify(response));
+
           updateRoom(msg.roomId);
 
           break;
         }
         case MessageType.LeaveRequest: {
-          delete rooms[msg.roomId].users[userId]
-          delete users[userId]
+          delete rooms[msg.roomId].users[userId];
+          delete users[userId];
 
-          updateRoom(msg.roomId)
+          updateRoom(msg.roomId);
 
           break;
         }
         case MessageType.SetVoteValue: {
-          rooms[msg.roomId].users[userId].voteValue = msg.payload.voteValue
+          rooms[msg.roomId].users[userId].voteValue = msg.payload.voteValue;
 
-          updateRoom(msg.roomId)
+          updateRoom(msg.roomId);
 
           break;
         }
         case MessageType.SetName: {
-          rooms[msg.roomId].users[userId].userName = msg.payload.name
+          rooms[msg.roomId].users[userId].userName = msg.payload.name;
 
-          updateRoom(msg.roomId)
+          updateRoom(msg.roomId);
 
           break;
         }
       }
-    } catch(error) {
-      console.error(error)
+    } catch (error) {
+      console.error(error);
       ws.send("Invalid message");
       ws.close();
     }
 
-    ws.on('close', () => {
+    ws.on("close", () => {
       // Remove user from users list
       delete users[userId];
 
       // Remove user from all rooms and update these rooms
       Object.keys(rooms).forEach((roomId) => {
-        if(rooms[roomId].users[userId]) {
+        if (rooms[roomId].users[userId]) {
           delete rooms[roomId].users[userId];
 
           updateRoom(roomId);
         }
       });
-    })
-
+    });
   });
-
 });
 
 //start our server
 server.listen(port, () => {
-    console.log(`Server started on port ${port} :)`);
+  console.log(`Server started on port ${port} :)`);
 });

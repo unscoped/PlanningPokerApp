a01591e8aae6f47ca74145eb9fd0088129ad3e40
okwast <okwast@users.noreply.github.com>
2020-05-14 19:48:30 +0000

    deploy: b6b1b65440f9e92da1b1b30afc6713a511b3af61

---
+++ precache-manifest.023012af86b363a38a907754ad00aa11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "75a7c3a652de8a7b0120",
+    "url": "/static/js/app.ed2a6181.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "9ea02c2ed402b432ca55",
+    "url": "/static/js/2.d094886c.chunk.js"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e10ce0552ee7802193950fa9181bd961",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "b333dcaccf681a69a1b0a5058aa06fcb",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "62273473911e68575ace8d48f6ebd399",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d50a7bd05718a779ebadcb692f265bf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b8b6bd227af1d2bd0c4a972b07ca25",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];

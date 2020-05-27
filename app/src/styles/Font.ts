3b832b732ef3df58323b092bc1368b66533fdc24
bonesyblue <bonesyblue@gmail.com>
2020-05-27 22:04:36 +0200

    chore: Add Poppins font to project 🔤

--- app/src/styles/Font.ts
+++ app/src/styles/Font.ts
@@ -2,66 +2,79 @@ import { StyleSheet } from 'react-native';
 
 export const fontStyles = StyleSheet.create({
   body1: {
+    fontFamily: 'Poppins',
     fontSize: 16,
     fontWeight: '400',
     letterSpacing: 0.5,
   },
   body2: {
+    fontFamily: 'Poppins',
     fontSize: 14,
     fontWeight: '400',
     letterSpacing: 0.25,
   },
   button: {
+    fontFamily: 'Poppins',
     fontSize: 14,
     fontWeight: '500',
     letterSpacing: 1.25,
   },
   caption: {
+    fontFamily: 'Poppins',
     fontSize: 12,
     fontWeight: '400',
     letterSpacing: 0.4,
   },
   headline1: {
+    fontFamily: 'Poppins',
     fontSize: 96,
     fontWeight: '300',
     letterSpacing: -1.5,
   },
   headline2: {
+    fontFamily: 'Poppins',
     fontSize: 60,
     fontWeight: '300',
     letterSpacing: -0.5,
   },
   headline3: {
+    fontFamily: 'Poppins',
     fontSize: 48,
     fontWeight: '400',
     letterSpacing: 0,
   },
   headline4: {
+    fontFamily: 'Poppins',
     fontSize: 34,
     fontWeight: '400',
     letterSpacing: 0.25,
   },
   headline5: {
+    fontFamily: 'Poppins',
     fontSize: 24,
     fontWeight: '400',
     letterSpacing: 0,
   },
   headline6: {
+    fontFamily: 'Poppins',
     fontSize: 20,
     fontWeight: '500',
     letterSpacing: 0.15,
   },
   overline: {
+    fontFamily: 'Poppins',
     fontSize: 10,
     fontWeight: '400',
     letterSpacing: 1.5,
   },
   subtitle1: {
+    fontFamily: 'Poppins',
     fontSize: 16,
     fontWeight: '400',
     letterSpacing: 0.15,
   },
   subtitle2: {
+    fontFamily: 'Poppins',
     fontSize: 14,
     fontWeight: '500',
     letterSpacing: 0.1,

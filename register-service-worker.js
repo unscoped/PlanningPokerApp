e99411092f684b0605ff3e0b6c875534ca186094
bonesyblue <bonesyblue@gmail.com>
2021-07-28 22:29:24 +0200

    Updates

--- register-service-worker.js
+++
@@ -1,1 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/PlanningPokerApp/expo-service-worker.js",{scope:"/PlanningPokerApp/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));

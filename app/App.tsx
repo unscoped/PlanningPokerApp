96fc3c1cfa40aa04adf1cee03196b61ba800cf74
Oliver Kwast <ok@unscoped.dev>
2021-08-01 16:11:16 +0200

    Removed unnecessary space in import

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
-import { useFonts } from '@use-expo/font';
-import AppLoading  from 'expo-app-loading';
-import React, { useCallback, useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFonts } from '@use-expo/font';
+import AppLoading from 'expo-app-loading';
+import React, { useCallback, useEffect, useState } from 'react';
 import { AppearanceProvider } from 'react-native-appearance';
 import { Provider as PaperProvider } from 'react-native-paper';
 
